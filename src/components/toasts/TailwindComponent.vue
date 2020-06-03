d9ffa8ca30f3f1767677e9a26994bd0e803fc13c
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-06-03 02:59:50 +0200

    :bug: (tailwind-component) fixed svg icon fill

--- src/components/toasts/TailwindComponent.vue
+++ src/components/toasts/TailwindComponent.vue
@@ -23,9 +23,10 @@
     <!-- Icon -->
     <svg
       :class="[position]"
-      class="block fill-current opacity-75 h-4 w-4"
+      class="block opacity-75 h-4 w-4"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
+      fill="#FFFFFF"
     >
       <path
         d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
