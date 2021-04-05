d1662d9c56abe916d74d352199bd726b229aeeec
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-05 21:53:12 +0200

    :sparkles: (injectComponent) improve injectComponent for Vue 3

---
+++ src/lib/injectComponent.ts
@@ -0,0 +1,37 @@
+import { VueMyToastsComponent, VueMyToastsOptions } from 'src/types'
+import { createApp } from 'vue-demi'
+import MyToasts from '../components/MyToasts.vue'
+
+/**
+ * Register toast handler component to root
+ * @param pluginOptions
+ * @returns {{reference: Vue | object | Record<never, any>, instance: Vue}}
+ */
+export default (
+  pluginOptions: VueMyToastsOptions,
+  userComponent: VueMyToastsComponent,
+) => {
+  // inject userComponent to the pluginOptions
+  const _props = { ...pluginOptions, component: userComponent }
+
+  // Create root element
+  const rootEl = document.createElement('div')
+
+  // Set root element id
+  rootEl.id = 'vue-my-toasts'
+
+  // Append component to body
+  document.body.appendChild(rootEl)
+
+  // Create toasts layer instance
+  const vueInstance = createApp(MyToasts, _props)
+
+  // Create component
+  const vueComponent = vueInstance.mount('#vue-my-toasts')
+
+  // Return created instances
+  return {
+    reference: vueInstance,
+    instance: vueComponent,
+  }
+}
