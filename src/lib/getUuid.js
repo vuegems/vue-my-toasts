aaf5594e8b88c4040aaaff488dafa8d0178fb263
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-05 21:49:30 +0200

    :fire: (prune) prepare for TypeScript support; prune js

--- src/lib/getUuid.js
+++
@@ -1,8 +0,0 @@
-/**
- * Get a toast uuid, useful when no `id` is provided by the user.
- *
- * @returns {string}
- */
-export default () => {
-  return 'toast-' + Date.now() + '-' + Math.floor(Math.random() * 10)
-}
